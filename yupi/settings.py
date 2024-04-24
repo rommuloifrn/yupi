4def56d715ced6fbec256e41812697a63f0174da
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-04-24 02:17:04 -0300

    build: add requirements.txt, configure django-browser-reload

--- yupi/settings.py
+++ yupi/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_browser_reload',
     'core'
 ]
 
@@ -43,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'yupi.urls'
